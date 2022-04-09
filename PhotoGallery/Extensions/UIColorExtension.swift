7524a0a49eca6fd3eae3d7c600a45643e148c032
yyulix <yuliarandomchenko@gmail.com>
2022-04-09 23:49:57 +0300

    Add extensions for app colors and icons

---
+++ PhotoGallery/Extensions/UIColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColorExtension.swift
+//  PhotoGallery
+//
+//  Created by Yulia Popova on 9/4/2022.
+//
+
+import UIKit
+
+extension UIColor {
+    struct AppColors {
+        static let accentColor = UIColor.systemOrange
+    }
+}
---
+++ PhotoGallery/Extensions/UIImageExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIImageExtension.swift
+//  PhotoGallery
+//
+//  Created by Yulia Popova on 9/4/2022.
+//
+
+import UIKit
+
+extension UIImage {
+    struct AuthIcons {
+        static var personIcon: UIImage { UIImage(systemName: "person")! }
+        static var lockIcon: UIImage { UIImage(systemName: "lock")! }
+    }
+}
