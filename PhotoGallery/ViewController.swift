02e5812a28f31c4735ebf75124453b855177143c
yyulix <yuliarandomchenko@gmail.com>
2022-04-09 17:15:39 +0300

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Yulia Popova on 9/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
